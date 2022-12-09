7a4fad146b30b08d80ceb9d1efc4908ade6e02aa
koudounasalkis <alkis.koudounas@polito.it>
2022-12-09 21:44:53 +0000

    1.4 encode episodes' description

---
+++ episodes_encoding.py
@@ -0,0 +1,14 @@
+from sentence_transformers import SentenceTransformer, util
+
+model_name = "all-mpnet-base-v2"    # "multi-qa-MiniLM-L6-cos-v1" (smaller and faster, but less accurate)
+model = SentenceTransformer(model_name)
+
+text_1 = "How big is London"
+text_2 = "London has 9,787,426 inhabitants at the 2011 census"
+text_3 = "London is known for its finacial district"
+list_text = [text_1, text_2, text_3]
+
+sentence_encodings = model.encode(list_text)
+
+print("Similarity:", util.dot_score(sentence_encodings[0], sentence_encodings[1]))
+print("Similarity:", util.dot_score(sentence_encodings[0], sentence_encodings[2]))
