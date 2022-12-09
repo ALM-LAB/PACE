934465cc23f40fec86ba77518a4c5833c46e484c
koudounasalkis <alkis.koudounas@polito.it>
2022-12-09 22:15:33 +0000

    1.4 encode episodes' description

--- episodes_encoding.py
+++ episodes_encoding.py
@@ -6,6 +6,9 @@ def episodes_encoding(list_text):
     sentence_encodings = model.encode(list_text)
     return sentence_encodings
 
+""" 
+
+Example Usage
 
 text_1 = "How big is London"
 text_2 = "London has 9,787,426 inhabitants at the 2011 census"
@@ -16,3 +19,5 @@ sentence_encodings = episodes_encoding(list_text)
 
 print("Similarity:", util.dot_score(sentence_encodings[0], sentence_encodings[1]))
 print("Similarity:", util.dot_score(sentence_encodings[0], sentence_encodings[2]))
+
+"""
