d1a8ba7b5c457f7f57456e964ee91ce32ddd5ce3
koudounasalkis <alkis.koudounas@polito.it>
2022-12-10 18:14:09 +0000

    comments on making gifs code

--- make_gifs.py
+++ make_gifs.py
@@ -1,13 +1,19 @@
 import glob
 from PIL import Image
 
+## Function to make gifs from images
 def make_gif(frame_folder):
+
+    ## Get all the folders in the frame folder
     folders = glob.glob(f"{frame_folder}/*")
+
+    ## For each folder, get all the images and make a gif
     for folder in folders:
         frames = [Image.open(image) for image in glob.glob(f"{folder}/*.png")]
         frame_one = frames[0]
         frame_one.save(f"{folder}/chapter.gif", format="GIF", append_images=frames,
                 save_all=True, duration=700, loop=0)
+    
     
 if __name__ == "__main__":
     make_gif("./images")
