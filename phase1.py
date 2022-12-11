d04fffe9620b79e45c58f3821a76676b7931f769
koudounasalkis <alkis.koudounas@polito.it>
2022-12-11 07:22:46 +0000

    comments on phase1 code

--- phase1.py
+++ phase1.py
@@ -1,49 +1,58 @@
-import pandas as pd
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-import spacy
-from sentence_transformers import SentenceTransformer
-from tqdm import tqdm
 import torch
+import spacy
+import pandas as pd
+from tqdm import tqdm
+
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from sentence_transformers import SentenceTransformer
 
 from clean_descriptions import description_cleaner
 from episodes_encoding import episodes_encoding
 from elasticsearch_index_episodes import elasticsearch_index_episodes 
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print("Using device:", device)
+if __name__ == "__main__":
 
-cleaner_model = AutoModelForSequenceClassification.from_pretrained('morenolq/spotify-podcast-advertising-classification')
-cleaner_model.to(device)
-tokenizer = AutoTokenizer.from_pretrained('morenolq/spotify-podcast-advertising-classification')
-nlp = spacy.load("en_core_web_sm")
-model_name = "all-mpnet-base-v2" # "multi-qa-MiniLM-L6-cos-v1" (smaller and faster, but less accurate)
-encoder_model = SentenceTransformer(model_name)
-encoder_model.to(device)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("Using device:", device)
 
+    ## Load the description cleaner model
+    cleaner_model = AutoModelForSequenceClassification.from_pretrained('morenolq/spotify-podcast-advertising-classification')
+    cleaner_model.to(device)
+    tokenizer = AutoTokenizer.from_pretrained('morenolq/spotify-podcast-advertising-classification')
+    nlp = spacy.load("en_core_web_sm")
 
-df_episodes = pd.read_csv("data/news_episodes.csv")
-episode_embedding_dict = {}
-description_clean_dict = {}
-episodes_id_to_remove = []
-for index in tqdm(df_episodes.index):
-    description = df_episodes.loc[index,"episode_description"]
-    episode_id = df_episodes.loc[index,"episode_id"]
-    try:
-        description = description_cleaner(cleaner_model, tokenizer, nlp, description, device)
-        episode_embedding = episodes_encoding(description, encoder_model)
-        episode_embedding_dict[episode_id] = episode_embedding
-        description_clean_dict[episode_id] = description
-    except Exception as e:
-        print (e)
-        print("Error with episode_id: ", episode_id)
-        # remove the episode from the dataframe
-        episodes_id_to_remove.append(episode_id)
+    ## Load the sentence transformer model
+    model_name = "all-mpnet-base-v2" # "multi-qa-MiniLM-L6-cos-v1" (smaller and faster, but less accurate)
+    encoder_model = SentenceTransformer(model_name)
+    encoder_model.to(device)
 
-    if index == 15000:
-        dense_dim = len(episode_embedding)
-        elasticsearch_index_episodes(df_episodes.head(15000), episode_embedding_dict, description_clean_dict, dense_dim)
+    ## Load the data
+    df_episodes = pd.read_csv("data/news_episodes.csv")
 
-df_episodes = df_episodes[~df_episodes.episode_id.isin(episodes_id_to_remove)]
+    ## Clean the descriptions and encode them
+    episode_embedding_dict = {}
+    description_clean_dict = {}
+    episodes_id_to_remove = []
+    for index in tqdm(df_episodes.index):
+        description = df_episodes.loc[index,"episode_description"]
+        episode_id = df_episodes.loc[index,"episode_id"]
+        try:
+            description = description_cleaner(cleaner_model, tokenizer, nlp, description, device)
+            episode_embedding = episodes_encoding(description, encoder_model)
+            episode_embedding_dict[episode_id] = episode_embedding
+            description_clean_dict[episode_id] = description
+        except Exception as e:
+            print (e)
+            print("Error with episode_id: ", episode_id)
+            episodes_id_to_remove.append(episode_id)
 
-dense_dim = len(episode_embedding)
-elasticsearch_index_episodes(df_episodes, episode_embedding_dict, description_clean_dict, dense_dim)
+        if index == 15000:
+            dense_dim = len(episode_embedding)
+            elasticsearch_index_episodes(df_episodes.head(15000), episode_embedding_dict, description_clean_dict, dense_dim)
+
+    ## Remove the episodes that could not be encoded
+    df_episodes = df_episodes[~df_episodes.episode_id.isin(episodes_id_to_remove)]
+
+    ## Index the episodes in Elasticsearch
+    dense_dim = len(episode_embedding)
+    elasticsearch_index_episodes(df_episodes, episode_embedding_dict, description_clean_dict, dense_dim)
